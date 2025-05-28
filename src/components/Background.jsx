dd66dee7cb7e558e102a21e5d279f833cf982dcc
Priyanshu579 <priyanshucorei5@gmail.com>
2025-05-28 23:12:10 +0530

    setbase of catimage

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -6,7 +6,7 @@ const Background = () => {
         <div className="fixed z-[2] top-[5%] w-full py-10 flex justify-center text-zinc-600 text-[30px] font-semibold">
           setYourTasks
         </div>
-          <img src="/cat.gif" alt="cat" className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]"/>
+          <img src="/animatedTodo/cat.gif" alt="cat" className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]"/>
       </div>
   );
 };
